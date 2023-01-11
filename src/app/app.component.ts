7dc8fef8e701b9996aa1e4798f080b0edd8f167b
Nguyen Nam <thenam97ls@gmail.com>
2023-01-11 20:08:53 +0700

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+//
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ProductAddComponent } from './Product/product-add/product-add.component';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FE';
+  
+  nameUser = 'Nguyen The Nam';
+
+  constructor(private dialog: MatDialog ){}
+
+  openDialogAdd(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus=true
+    dialogConfig.width = "50%"
+    const dialogRef = this.dialog.open(ProductAddComponent,dialogConfig)
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//
+import {MatDialogModule} from "@angular/material/dialog"; 
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+import { ProductListComponent } from './Product/product-list/product-list.component';
+import { ProductEditComponent } from './Product/product-edit/product-edit.component';
+import { ProductAddComponent } from './Product/product-add/product-add.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductEditComponent,
+    ProductAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents:[]
 })
 export class AppModule { }
